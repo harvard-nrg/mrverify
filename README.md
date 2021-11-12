77601070c527a759927988624b2ee8e0aed47ac0
Timothy O'Keefe <timothy_okeefe@harvard.edu>
2021-11-12 18:57:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Mr. Verify: Verify those scan parameters
+========================================
+Mr. Verify (or "MR verify") is a configurable command line tool to verify scan 
+parameters within DICOM images, generate fancy HTML reports, and send notifications 
+when there are verification issues.
+
+## Table of contents
+1. [Documentation](#documentation)
+2. [Installation](#installation)
+3. [Configuration](#configuration)
+
+## Documentation
+coming soon
+
+## Installation
+Just `pip`
+
+```bash
+python -m pip install mrverify
+```
+
+## Configuration
+You can find an example configuration file [here](https://github.com/harvard-nrg/mrverify/blob/main/mrverify/configs/ssbc.yaml).
---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires = [
+    'yaxil',
+    'pyaml',
+    'jinja2'
+]
+
+test_requirements = [
+    'pytest',
+    'vcrpy'
+]
+
+about = dict()
+with open(os.path.join(here, 'mrverify', '__version__.py'), 'r') as f:
+    exec(f.read(), about)
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    packages=find_packages(),
+    package_data={
+        '': ['*.yaml', '*.html']
+    },
+    include_package_data=True,
+    scripts=[
+        'scripts/mrcheck.py'
+    ],
+    install_requires=requires,
+    tests_require=test_requirements
+)
+
